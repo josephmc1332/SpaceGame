0740e633ff7f88270b0f6e36969dd3ab2ca18d8f
Jacob Grimm <jacob.d.grimm@gmail.com>
2018-08-20 15:57:40 -0400

    added the beginings of the story

--- SpaceGame/IntroPage.cs
+++ SpaceGame/IntroPage.cs
@@ -16,7 +16,7 @@ namespace SpaceGame
         public void FirstPage()
         {
             //ask user for name
-            Console.WriteLine("Hello, welcome to Space Game!\n-----------------------------------\n" +
+            Console.WriteLine("Hello, welcome to Space Game!\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n" +
                 " What is your name?");
             string yourName = Console.ReadLine();
             //clear code
@@ -25,7 +25,9 @@ namespace SpaceGame
 
 
             //add story here
-            Console.WriteLine($"Okay, {yourName} ");
+            Console.WriteLine($"Okay, {yourName}. You were engaged to Venusian royalty but the king has forbidden your beloved to marry a mere commoner like yourself." +
+                $"But there is even worse news! Your beloved has other interested parties, and what's worse is they are already nobility. But you are in luck" +
+                $" there is a way to buy into galactic nobility, but it's going to be a lot of work, so get out there and get to trading {yourName}!");
 
             Console.ReadLine();
 
