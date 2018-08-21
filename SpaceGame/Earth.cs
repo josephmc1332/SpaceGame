de1c26e5ffad4f95faa1bdb0d25cfb91a35555e6
Josephmc1332 <Josephmc1332@gmail.com>
2018-08-21 09:22:36 -0400

    Added properties in PersonalStatus class

--- SpaceGame/Earth.cs
+++ SpaceGame/Earth.cs
@@ -11,50 +11,73 @@ namespace SpaceGame
         #region Earth Page
         public void EarthPage()
         {
+            //clear up window
             Console.Clear();
+
+            //display menu on earth upon arrival
             Console.WriteLine("Welcome to Earth! Where would you like to go? \n" +
                 "1. Ship Yard \n" +
                 "2. Galactic Bank \n" +
                 "3. Buy, Sell, Trade \n" +
                 "4. Galactic Market\n" +
                 "5. Departure Port");
+
+            //send back to check selected option after invalid input
             try
             {
                 SelectEarthOptions();
             }
+
+            //display if invalid input
             catch (Exception)
             {
                 Console.WriteLine("Invalid input, try again! Press any key to continue.");
                 Console.ReadLine();
+
+                //recycle to Welcome to Earth after invalid entry  
                 EarthPage();
             }
-            // add exception for invalid answer
+            
 
 
         }
         public void SelectEarthOptions()
         {
+            //convert/parse user input string 
             int response = Convert.ToInt32(Console.ReadLine());
+
+            //evaluate user input by using boolean expressions
             bool shipYard = response == 1;
             bool galacticBank = response == 2;
             bool shop = response == 3;
             bool market = response == 4;
             bool port = response == 5;
+            
+
+            //point of method access after valid user selection
             if (shipYard) ;
             //  ShipYard();
+
             if (galacticBank) ;
             //   GalacticBank();
+
             if (shop) ;
             //   Shop();
+
             if (market) ;
             //   Market();
+
             if (port) ;
             //   Port();
+
             else
             {
+                //loops back to the beginning of earth page
                 Console.WriteLine("invalid entry");
                 EarthPage();
             }
+
+
 
         }
         #endregion
--- SpaceGame/PersonalStatus.cs
+++ SpaceGame/PersonalStatus.cs
@@ -8,18 +8,54 @@ namespace SpaceGame
 {
     class PersonalStatus
     {
+        /// <summary>
+        /// Personal Info
+        /// </summary>
         public int MyCurrentCredit { get; set; }
 
-        public string YourName { get; set; }
+        public string MyName { get; set; }
 
+        public int MyTravelTime { get; set; }
 
-        
-
-
-
+        public string MyCurrentLocation { get; set; }
 
 
 
-        
+        /// <summary>
+        /// Ship Specs
+        /// </summary>
+        public string ShipName { get; set; }
+
+        public int ShipSpeed { get; set; }
+
+        public int ShipCapacity { get; set; }
+
+
+
+        /// <summary>
+        /// cargo
+        /// </summary>
+        public int SpaceGold { get; set; }
+
+        public int NoBalanaceShoes { get; set; }
+
+        public int GalacticTVs { get; set; }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
