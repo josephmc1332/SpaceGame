955ced123da901f8ae2e805e9433f35647b9237b
Jacob Grimm <jacob.d.grimm@gmail.com>
2018-08-28 13:51:19 -0400

    adds Asgard to Planet Info class

--- SpaceGame/PlanetInfo.cs
+++ SpaceGame/PlanetInfo.cs
@@ -8,6 +8,7 @@ namespace SpaceGame
 {
     class PlanetInfo
     {
+        Random rnd = new Random();
         public PlanetInfo()
         {
 
@@ -15,87 +16,97 @@ namespace SpaceGame
 
 
             //cost on earth
-            this.EarthSpaceGold = 100;
+            this.EarthSpaceGold      = 100;
 
             this.EarthNoBalanceShoes = 80;
 
-            this.EarhtGalacticTVs = 120;
+            this.EarhtGalacticTVs    = 120;
 
 
 
             //cost on alphaC
-            this.AlphaCentariGalacticTVs = 100;
+            this.AlphaCentariGalacticTVs    = 100;
 
-            this.AlphaCentariGold = 150;
+            this.AlphaCentariGold           = 150;
 
             this.AlphaCentariNoBalanceShoes = 65;
 
 
             //cost on M63
-            this.M63GalacticTVs = 140;
+            this.M63GalacticTVs    = 140;
 
             this.M63NoBalanceShoes = 100;
 
-            this.M63SpaceGold = 60;
+            this.M63SpaceGold      = 60;
 
-            
+
             //cost on Asgard
-            this.AsgardGalacticTVs = 170;
-
+            // There aren't even any TVs in Asgard yet, isn't "globalization" grand
+            this.AsgardGalacticTVs    = 170;
+            // Asgardians are amazed by the new zero gravity shoe technology
             this.AsgardNoBalanceShoes = 150;
-
-            this.AsgardGold = 15;
+            // Asgard is basically made of space gold so there is no need for them to buy it
+            this.AsgardGold           = 15;
 
 
             //Earth Location
-            this.EarthXPosition = 0;
+            this.EarthXPosition = 0.0;
 
-            this.EarthYPosition = 0;
+            this.EarthYPosition = 0.0;
 
 
             //AlphaC Location
-            this.AlphaCentariXPosition = 0;
+            this.AlphaCentariXPosition = 0.0;
 
             this.AlphaCentariYPosition = -4.37;
 
 
             //M63 Location
-            this.M63XPosition = 6;
+            this.M63XPosition = 6.0;
 
-            this.M63YPosition = 4;
+            this.M63YPosition = 4.0;
+
+
+            //Asgard Location
+            double axpositon = rnd.Next(8, 11);
+            this.AsgardXPosition = axpositon;
+
+            double ayposition = rnd.Next(5, 7);
+            this.AsgardYPosition = ayposition;
         }
 
         /// <summary>
         /// cargo
         /// </summary>
         
-        public int EarthSpaceGold { get; private set; }
+        public int EarthSpaceGold   { get; private set; }
         public int AlphaCentariGold { get; private set; }
-        public int M63SpaceGold { get; private set; }
-        public int AsgardGold { get; private set; }
+        public int M63SpaceGold     { get; private set; }
+        public int AsgardGold       { get; private set; }
 
 
-        public int EarthNoBalanceShoes { get; private set; }
-        public int AlphaCentariNoBalanceShoes { get; private set; }
-        public int M63NoBalanceShoes { get; private set; }
-        public int AsgardNoBalanceShoes { get; set; }
+        public int EarthNoBalanceShoes          { get; private set; }
+        public int AlphaCentariNoBalanceShoes   { get; private set; }
+        public int M63NoBalanceShoes            { get; private set; }
+        public int AsgardNoBalanceShoes         { get; set; }
 
-        public int EarhtGalacticTVs { get; private set; }
-        public int AlphaCentariGalacticTVs { get; private set; }
-        public int M63GalacticTVs { get; private set; }
-        public int AsgardGalacticTVs { get; set; }
+        public int EarhtGalacticTVs         { get; private set; }
+        public int AlphaCentariGalacticTVs  { get; private set; }
+        public int M63GalacticTVs           { get; private set; }
+        public int AsgardGalacticTVs        { get; set; }
 
         /// <summary>
         /// planetary positions
         /// </summary>
-        public double EarthXPosition { get; private set; }
-        public double EarthYPosition { get; private set; }
+        public double EarthXPosition        { get; private set; }
+        public double EarthYPosition        { get; private set; }
         public double AlphaCentariXPosition { get; private set; }
         public double AlphaCentariYPosition { get; private set; }
-        public double M63XPosition { get; private set; }
-        public double M63YPosition { get; private set; }
+        public double M63XPosition          { get; private set; }
+        public double M63YPosition          { get; private set; }
+        public double AsgardXPosition       { get; private set; }
+        public double AsgardYPosition       { get; private set; }
 
-        
 
 
 
