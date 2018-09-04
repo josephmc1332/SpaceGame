38fd82b8fd671c5370eded047b4c476f7941c9e3
Jacob Grimm <jacob.d.grimm@gmail.com>
2018-09-03 21:42:53 -0400

    costs for picium

--- SpaceGame/PlanetInfo.cs
+++ SpaceGame/PlanetInfo.cs
@@ -16,46 +16,46 @@ namespace SpaceGame
 
 
             //cost on earth
-            this.EarthSpaceGold      = 100;
+            this.EarthSpaceGold = 100;
 
             this.EarthNoBalanceShoes = 80;
 
-            this.EarthGalacticTVs    = 120;
+            this.EarthGalacticTVs = 120;
 
 
 
             //cost on alphaC
-            this.AlphaCentariGalacticTVs    = 100;
+            this.AlphaCentariGalacticTVs = 100;
 
-            this.AlphaCentariGold           = 150;
+            this.AlphaCentariGold = 150;
 
             this.AlphaCentariNoBalanceShoes = 65;
 
 
             //cost on M63
-            this.M63GalacticTVs    = 140;
+            this.M63GalacticTVs = 140;
 
             this.M63NoBalanceShoes = 100;
 
-            this.M63SpaceGold      = 60;
+            this.M63SpaceGold = 60;
 
 
             //cost on Asgard
             // There aren't even any TVs in Asgard yet, isn't "globalization" grand
-            this.AsgardGalacticTVs    = 170;
+            this.AsgardGalacticTVs = 170;
             // Asgardians are amazed by the new zero gravity shoe technology
             this.AsgardNoBalanceShoes = 150;
             // Asgard is basically made of space gold so there is no need for them to buy it
-            this.AsgardGold           = 15;
+            this.AsgardGold = 15;
 
 
             //cost on Planet X
             //nearly the same economy of earth, slightly richer in mineral resourses but slightly more expensive in manufactured goods
-            this.PlanetXGold           = 90;
+            this.PlanetXGold = 90;
 
             this.PlanetXNoBalanceShoes = 90;
 
-            this.PlanetXGalacticTVs    = 130;
+            this.PlanetXGalacticTVs = 130;
             // we will make fuel cheap here
 
             //cost on Planet joe
@@ -80,6 +80,16 @@ namespace SpaceGame
 
             this.VormirGold = 250;
 
+            //cost on Picium
+            int pshoe = rnd.Next(300, 475);
+            int ptv = rnd.Next(400, 500);
+            int pgold = rnd.Next(300, 700);
+
+            this.PiciumNoBalanaceShoes = pshoe;
+
+            this.PiciumGold = pgold;
+
+            this.PiciumGalacticTVs = ptv;
 
 
 
@@ -102,10 +112,10 @@ namespace SpaceGame
 
 
             //Asgard Location
-            double axpositon     = rnd.Next(8, 13);
+            double axpositon = rnd.Next(8, 13);
             this.AsgardXPosition = axpositon;
 
-            double ayposition    = rnd.Next(5, 9);
+            double ayposition = rnd.Next(5, 9);
             this.AsgardYPosition = ayposition;
 
 
@@ -129,6 +139,7 @@ namespace SpaceGame
             double typosition = rnd.Next(6, 12);
             this.TitanYPosition = typosition;
 
+
             //Vormir Position
             double vormirXPosition = rnd.Next(13, 17);
             this.VormirXPosition = vormirXPosition;
@@ -136,7 +147,11 @@ namespace SpaceGame
             double vormirYPosition = rnd.Next(2, 10);
             this.VormirYPosition = vormirYPosition;
 
-            
+            double pxposition = rnd.Next(15, 25);
+            this.PiciumXPosition = pxposition;
+
+            double pyposition = rnd.Next(-7, 3);
+            this.PiciumYPosition = pyposition;
         }
 
 
@@ -144,51 +159,65 @@ namespace SpaceGame
         /// cargo
         /// </summary>
 
-        public int EarthSpaceGold   { get; private set; }
+        public int EarthSpaceGold { get; private set; }
         public int AlphaCentariGold { get; private set; }
-        public int M63SpaceGold     { get; private set; }
-        public int AsgardGold       { get; private set; }
-        public int PlanetXGold      { get; private set; }
-        public int PlanetJoeGold    { get; private set; }
-        public int TitanGold        { get; private set; }
-        public int VormirGold       { get; private set; }
+        public int M63SpaceGold { get; private set; }
+        public int AsgardGold { get; private set; }
+        public int PlanetXGold { get; private set; }
+        public int PlanetJoeGold { get; private set; }
+        public int TitanGold { get; private set; }
+        public int VormirGold { get; private set; }
+        public int PiciumGold { get; private set; }
 
-        public int EarthNoBalanceShoes          { get; private set; }
-        public int AlphaCentariNoBalanceShoes   { get; private set; }
-        public int M63NoBalanceShoes            { get; private set; }
-        public int AsgardNoBalanceShoes         { get; private set; }
-        public int PlanetXNoBalanceShoes        { get; private set; }
-        public int PlanetJoeNoBalanceShoes      { get; private set; }
-        public int TitanNoBalanceShoes          { get; private set; }
-        public int VormirNoBalanceShoes         { get; private set; }
+        public int EarthNoBalanceShoes { get; private set; }
+        public int AlphaCentariNoBalanceShoes { get; private set; }
+        public int M63NoBalanceShoes { get; private set; }
+        public int AsgardNoBalanceShoes { get; private set; }
+        public int PlanetXNoBalanceShoes { get; private set; }
+        public int PlanetJoeNoBalanceShoes { get; private set; }
+        public int TitanNoBalanceShoes { get; private set; }
+        public int VormirNoBalanceShoes { get; private set; }
+        public int PiciumNoBalanaceShoes { get; private set; }
 
-        public int EarthGalacticTVs         { get; private set; }
-        public int AlphaCentariGalacticTVs  { get; private set; }
-        public int M63GalacticTVs           { get; private set; }
-        public int AsgardGalacticTVs        { get; private set; }
-        public int PlanetXGalacticTVs       { get; private set; }
-        public int PlanetJoeGalacticTVs     { get; private set; }
-        public int TitanGalacticTVs         { get; private set; }
-        public int VormirGalacticTVs        { get; private set; }
+        public int EarthGalacticTVs { get; private set; }
+        public int AlphaCentariGalacticTVs { get; private set; }
+        public int M63GalacticTVs { get; private set; }
+        public int AsgardGalacticTVs { get; private set; }
+        public int PlanetXGalacticTVs { get; private set; }
+        public int PlanetJoeGalacticTVs { get; private set; }
+        public int TitanGalacticTVs { get; private set; }
+        public int VormirGalacticTVs { get; private set; }
+        public int PiciumGalacticTVs { get; private set; }
+
         /// <summary>
         /// planetary positions
         /// </summary>
-        public double EarthXPosition        { get; private set; }
-        public double EarthYPosition        { get; private set; }
+        public double EarthXPosition { get; private set; }
+        public double EarthYPosition { get; private set; }
         public double AlphaCentariXPosition { get; private set; }
         public double AlphaCentariYPosition { get; private set; }
-        public double M63XPosition          { get; private set; }
-        public double M63YPosition          { get; private set; }
-        public double AsgardXPosition       { get; private set; }
-        public double AsgardYPosition       { get; private set; }
-        public double PlanetXXPosition      { get; private set; }
-        public double PlanetXYPosition      { get; private set; }
-        public double PlanetJoeXPosition    { get; private set; }
-        public double PlanetJoeYPosition    { get; private set; }
-        public double TitanXPosition        { get; private set; }
-        public double TitanYPosition        { get; private set; }
-        public double VormirXPosition       { get; private set; }
-        public double VormirYPosition       { get; private set; }
+        public double M63XPosition { get; private set; }
+        public double M63YPosition { get; private set; }
+        public double AsgardXPosition { get; private set; }
+        public double AsgardYPosition { get; private set; }
+        public double PlanetXXPosition { get; private set; }
+        public double PlanetXYPosition { get; private set; }
+        public double PlanetJoeXPosition { get; private set; }
+        public double PlanetJoeYPosition { get; private set; }
+        public double TitanXPosition { get; private set; }
+        public double TitanYPosition { get; private set; }
+        public double VormirXPosition { get; private set; }
+        public double VormirYPosition { get; private set; }
+        public double PiciumXPosition { get; private set; }
+        public double PiciumYPosition { get; private set; }
+
+
+
+
+
+
+
+
 
 
     }
